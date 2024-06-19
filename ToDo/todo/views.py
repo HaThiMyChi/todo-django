78a6176de5873aa400c4a018e754eb26109dc4a3
MyChi <mychikhtn@gmail.com>
2024-06-19 14:08:52 +0700

    Mark as done function

--- ToDo/todo/views.py
+++ ToDo/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 
@@ -9,4 +9,12 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     # return HttpResponse('The form is submitted')
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    # primary key
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    # return HttpResponse(task)
     return redirect('home')
