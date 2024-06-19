8313aaef773ef68029e1808eaa78ead043a81527
MyChi <mychikhtn@gmail.com>
2024-06-19 16:55:21 +0700

    delete task

--- ToDo/todo/urls.py
+++ ToDo/todo/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # Mark as undone
     path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
     # Edit feature
-    path('edit_task/<int:pk>/', views.edit_task, name='edit_task')
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # Delete feature
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task')
 ]   
