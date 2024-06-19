ce8586c669494b5dfcbedc635c11275be7aff53d
MyChi <mychikhtn@gmail.com>
2024-06-19 11:09:36 +0700

    Add task function

--- ToDo/todo_main/urls.py
+++ ToDo/todo_main/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name="home"),
 
     # To do
     path('todo/', include('todo.urls'))
--- ToDo/todo_main/views.py
+++ ToDo/todo_main/views.py
@@ -4,7 +4,7 @@ from todo.models import Task
 
 def home(request):
     # return HttpResponse('<h1>Homepage</h1>')
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     print(tasks)
     context = {
         'tasks':tasks,
